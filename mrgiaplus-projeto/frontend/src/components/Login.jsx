85b644999d94aade0134902a43a703fb934b31c4
mrgrafael <mrg.rafaeldietrich@gmail.com>
2025-04-07 00:06:37 -0300

    Add files via upload

---
+++ mrgiaplus-projeto/frontend/src/components/Login.jsx
@@ -0,0 +1,9 @@
+export default function Login() {
+  return (
+    <div>
+      <h1>Login</h1>
+      <p>Por favor, clique no botão para fazer login.</p>
+      <button>Entrar</button>
+    </div>
+  )
+}
