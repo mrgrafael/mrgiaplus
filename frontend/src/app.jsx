94612c1bef1de56d381b319d1733f74d349e2a78
mrgrafael <mrg.rafaeldietrich@gmail.com>
2025-05-05 13:12:38 -0300

    app.jsx

---
+++ frontend/src/app.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/Login.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
+  );
+}
+
+export default App;
